49cffa9d05f1b2e81d692c3f804861b9e6e5835f
Dhruv Parekh <parekhdhruv04@gmail.com>
2023-05-05 13:34:05 +0530

    Fix axios function code

--- functions/axios-demo.js
+++ functions/axios-demo.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
-    const data = await axios.get("https://jsonplaceholder.typicode.com/users");
-    console.log(data);
-    res.status(200).send(data);
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    res.status(200).send(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).end();
